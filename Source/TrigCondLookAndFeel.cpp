02e2342cf94eeae1b2f185110f967111e4b0b346
Alfred Kristal Ern <alfred.kristal@gmail.com>
2022-09-06 13:41:04 +0200

    added look and feel to customize slider appearance

---
+++ Source/TrigCondLookAndFeel.cpp
@@ -0,0 +1,21 @@
+/*
+  ==============================================================================
+
+    TrigCondLookAndFeel.cpp
+    Created: 6 Sep 2022 1:33:03pm
+    Author:  Alfred Kristal-Ern
+
+  ==============================================================================
+*/
+
+#include "TrigCondLookAndFeel.h"
+
+void TrigCondLookAndFeel::drawRotarySlider (juce::Graphics &,
+                                            int x, int y,
+                                            int width,int height,
+                                            float sliderPosProportional,
+                                            float rotaryStartAngle,
+                                            float rotaryEndAngle, juce::Slider &)
+{
+    
+}
---
+++ Source/TrigCondLookAndFeel.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    TrigCondLookAndFeel.h
+    Created: 6 Sep 2022 1:33:03pm
+    Author:  Alfred Kristal-Ern
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+class TrigCondLookAndFeel : public juce::LookAndFeel_V4 {
+public:
+    virtual void drawRotarySlider (juce::Graphics &, int x, int y, int width, int height, float sliderPosProportional, float rotaryStartAngle, float rotaryEndAngle, juce::Slider &) override;
+};
