0b38e0b399d8cec65cd24a12904163a1ecf5f6f7
Alfred Kristal Ern <alfred.kristal@gmail.com>
2022-09-07 11:19:24 +0200

    render labels

--- Source/TrigCondLookAndFeel.cpp
+++ Source/TrigCondLookAndFeel.cpp
@@ -47,4 +47,6 @@ void TrigCondLookAndFeel::drawRotarySlider (juce::Graphics& g,
 
 void TrigCondLookAndFeel::drawLabel(juce::Graphics& g, juce::Label& label)
 {
+    g.setColour(juce::Colours::white);
+    g.drawText(label.getText(), 0, 0, label.getWidth(), label.getHeight(), juce::Justification::centred);
 }
